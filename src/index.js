604cb1b84e8dbef59444322f6a83df23790215c0
Artsemi <stojanka25@gmail.com>
2022-02-19 00:22:53 +0300

    refactor: make resolvment shorter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ module.exports = function towelSort (matrix) {
   if (!matrix || matrix.length === 0){
     return [];
   } else{
-    let newMatrix = matrix.map((el, index) => index % 2 === 0 ? el : el.reverse());
-    return newMatrix.flat();
+    return matrix.map((el, index) => index % 2 === 0 ? el : el.reverse()).flat();
   }
 }
